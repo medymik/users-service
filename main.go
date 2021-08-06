506c6c784de25db60e3048718bf69560d91e286b
mohammed ymik <mohammed.ymik@gmail.com>
2021-08-06 11:28:35 +0100

    add gorilla mux

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/medymik/configo/env"
 )
 
 func main() {
+	// Load env from file
 	env := env.NewEnv(".env")
 	env.Load()
-	fmt.Println(os.Getenv("DB_NAME"))
+	// New mux
+	r := mux.NewRouter()
+	// Server
+	s := &http.Server{
+		Addr:    os.Getenv("PORT"),
+		Handler: r,
+	}
+	s.ListenAndServe()
 }

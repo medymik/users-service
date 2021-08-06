f3b3b08fbbbc44cada2ceeecd676bcd095a9755a
mohammed ymik <mohammed.ymik@gmail.com>
2021-08-06 11:37:43 +0100

    add logger as a dp and register handlers function

--- main.go
+++ main.go
@@ -4,17 +4,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"users-service/handlers"
 
 	"github.com/gorilla/mux"
 	"github.com/medymik/configo/env"
 )
 
+func registerHandlers(r *mux.Router, l *log.Logger) {
+	r.Handle("/api/users", handlers.NewUser(l))
+}
+
 func main() {
+	// New logger
+	l := log.New(os.Stdout, "users-service", log.LstdFlags)
 	// Load env from file
 	env := env.NewEnv(".env")
 	env.Load()
 	// New mux
 	r := mux.NewRouter()
+	// register handlers
+	registerHandlers(r, l)
 	// Server
 	s := &http.Server{
 		Addr:    ":" + os.Getenv("PORT"),
